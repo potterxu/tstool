6ddd434f8862c110a6a1e7a213469b1ca8a0020a
Potter Xu <xujingchuan1995@gmail.com>
2021-11-03 06:53:35 -0400

    Fill fields in adaptation

--- mpts/adaptation.go
+++ mpts/adaptation.go
@@ -5,7 +5,25 @@ import "github.com/potterxu/tstool/bitreader"
 type AdaptationType struct {
 	data []byte
 
-	DI bool // Discontinuity Indicator
+	DI                bool // Discontinuity Indicator
+	RAI               bool
+	ESPI              bool
+	PCRFlag           bool
+	OPCRFlag          bool
+	SplicingPointFlag bool
+	PrivateDataFlag   bool
+	ExtensionFlag     bool
+
+	//todo
+	PCR             int64
+	OPCR            int64
+	SpliceCountdown int
+
+	PrivateDataLength int
+	// PrivateData pointer
+
+	ExtensionLength int
+	// Extension pointer
 }
 
 func adaptation(data []byte, r *bitreader.BitReaderType) *AdaptationType {
@@ -18,4 +36,49 @@ func adaptation(data []byte, r *bitreader.BitReaderType) *AdaptationType {
 
 func (adapt *AdaptationType) parse(r *bitreader.BitReaderType) {
 	adapt.DI = r.ReadBit()
+	adapt.RAI = r.ReadBit()
+	adapt.ESPI = r.ReadBit()
+	adapt.PCRFlag = r.ReadBit()
+	adapt.OPCRFlag = r.ReadBit()
+	adapt.SplicingPointFlag = r.ReadBit()
+	adapt.PrivateDataFlag = r.ReadBit()
+	adapt.ExtensionFlag = r.ReadBit()
+
+	remainSize := len(adapt.data) - 1
+
+	if adapt.PCRFlag {
+		base, ext := r.ReadPCR()
+		adapt.PCR = base*300 + ext
+		remainSize -= 6
+	}
+
+	if adapt.OPCRFlag {
+		base, ext := r.ReadPCR()
+		adapt.OPCR = base*300 + ext
+		remainSize -= 6
+	}
+
+	if adapt.SplicingPointFlag {
+		adapt.SpliceCountdown = r.ReadBits(8)
+		remainSize -= 1
+	}
+
+	if adapt.PrivateDataFlag {
+		adapt.PrivateDataLength = r.ReadBits(8)
+		remainSize -= 1
+
+		r.SkipBytes(adapt.PrivateDataLength)
+		remainSize -= adapt.PrivateDataLength
+	}
+
+	if adapt.ExtensionFlag {
+		adapt.ExtensionLength = r.ReadBits(8)
+		remainSize -= 1
+
+		r.SkipBytes(adapt.ExtensionLength)
+		remainSize -= adapt.ExtensionLength
+	}
+
+	// stuffing
+	r.SkipBytes(remainSize)
 }
