1b13ec498afd852c8dd98625145a1b41f40a20d0
Potter Xu <xujingchuan1995@gmail.com>
2021-12-12 19:27:50 +0800

    [io] Add FileReader.

--- io/file.go
+++ io/file.go
@@ -2,6 +2,7 @@ package io
 
 import (
 	"bufio"
+	"io"
 	"os"
 
 	"github.com/potterxu/tstool/util"
@@ -9,6 +10,7 @@ import (
 
 const (
 	DEFAULT_BUFFER_SIZE int = 188 * 1000
+	READ_BYTES              = 188
 )
 
 type FileWriterType struct {
@@ -44,4 +46,40 @@ func (w *FileWriterType) Close() {
 func (w *FileWriterType) Write(data []byte) {
 	_, err := w.writer.Write(data)
 	util.LogOnErr(err)
+}
+
+type FileReaderType struct {
+	filename   string
+	bufferSize int
+
+	file   *os.File
+	reader *bufio.Reader
+}
+
+func FileReader(filename string) *FileReaderType {
+	return &FileReaderType{
+		filename:   filename,
+		bufferSize: DEFAULT_BUFFER_SIZE,
+	}
+}
+
+func (w *FileReaderType) Open() error {
+	var err error
+	w.file, err = os.Open(w.filename)
+	if err != nil {
+		return err
+	}
+	w.reader = bufio.NewReaderSize(w.file, w.bufferSize)
+	return nil
+}
+
+func (w *FileReaderType) Close() {
+	w.file.Close()
+}
+
+func (w *FileReaderType) Read() ([]byte, bool) {
+	data := make([]byte, READ_BYTES)
+	n, err := io.ReadFull(w.reader, data)
+
+	return data, err == nil && n == READ_BYTES
 }
