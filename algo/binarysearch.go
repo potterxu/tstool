e09c9c2bf4a8796d2357daba3edf9a7fd2d0e41c
Potter Xu <xujingchuan1995@gmail.com>
2021-12-23 10:50:17 +0000

    [statCmd] Support higher refreshing frequency.

--- algo/binarysearch.go
+++ algo/binarysearch.go
@@ -1,6 +1,6 @@
 package algo
 
-// Find largest index that data[index] <= target
+// Find index with largest data[index] with data[index] <= target
 func BinarySearchNoLargerThan(data []int64, target int64) int {
 	start := 0
 	end := len(data) - 1
@@ -13,4 +13,19 @@ func BinarySearchNoLargerThan(data []int64, target int64) int {
 		}
 	}
 	return end
+}
+
+// Find index with smallest data[index] with data[index] >= target
+func BinarySearchNoSmallerThan(data []int64, target int64) int {
+	start := 0
+	end := len(data) - 1
+	for start <= end {
+		mid := (end-start)/2 + start
+		if data[mid] < target {
+			start = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
+	return start
 }
